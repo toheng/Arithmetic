9fc82f9ab23f3dfa23c146baaf2ef8633e57b886
hengo <toheng@foxmail.com>
2018-07-26 23:59:04 +0800

    commit

--- Solution3.java
+++ Solution3.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 /**
  * 用两个栈实现队列
- * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ * 题目描述：用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
  */
 public class Solution {
     Stack<Integer> stack1 = new Stack<Integer>();
